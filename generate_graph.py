efc9ef1c7c948d6d0c20655440fa8c5f7fcde242
AlinaLadybug <alinaladybug23@gmail.com>
2019-05-12 21:41:27 +0300

    fix loops and add constants

--- generate_graph.py
+++ generate_graph.py
@@ -1,5 +1,8 @@
 import random
 
+MIN_EDGE_WEIGHT = 1
+MAX_EDGE_WEIGHT = 20
+
 
 def main():
     try:
@@ -15,7 +18,7 @@ def generate_graph(vertex_count, edge_count):
     if edge_count < vertex_count - 1:
         raise ValueError("Please, enter correct amount of edges")
     matrix = [[0 for x in range(vertex_count)] for y in range(vertex_count)]
-    while edge_count > 0:
+    for edge in range(edge_count):
         isolated_vertex = get_isolated_vertex(matrix)
         if isolated_vertex:
             create_edge(matrix, isolated_vertex)
@@ -23,18 +26,13 @@ def generate_graph(vertex_count, edge_count):
         else:
             random_vertex = random.randint(0, vertex_count-1)
             create_edge(matrix, random_vertex)
-        edge_count -= 1
     print_matrix(matrix)
 
 
 def get_isolated_vertex(matrix):
-    len_matrix = len(matrix)
-    for x in range(len_matrix):
-        row_sum = 0
-        for y in range(len_matrix):
-            row_sum += matrix[x][y]
-        if row_sum == 0:
-            return x
+    for i, row in enumerate(matrix):
+        if not sum(row):
+            return i
 
 
 def create_edge(matrix, start_vertex):
@@ -42,9 +40,7 @@ def create_edge(matrix, start_vertex):
     end_vertex = random.randint(0, len_matrix-1)
     while end_vertex == start_vertex or matrix[start_vertex][end_vertex] != 0:
         end_vertex = random.randint(0, len_matrix-1)
-    min_random_value = 1
-    max_random_value = 20
-    value = random.randint(min_random_value, max_random_value)
+    value = random.randint(MIN_EDGE_WEIGHT, MAX_EDGE_WEIGHT)
     matrix[start_vertex][end_vertex] = value
     matrix[end_vertex][start_vertex] = value
 
